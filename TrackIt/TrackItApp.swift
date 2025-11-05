7f7bbb9a0e128e084af2875854520e732aa613b9
gagasslut <152082235+Walk3r11@users.noreply.github.com>
2025-11-05 21:49:50 +0200

    Initial Xcode project commit

---
+++ TrackIt/TrackItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackItApp.swift
+//  TrackIt
+//
+//  Created by Konstantin Nikolow on 5.11.25.
+//
+
+import SwiftUI
+
+@main
+struct TrackItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
